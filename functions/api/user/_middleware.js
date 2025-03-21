7810b4789798a08e2cd3fee3063d5d7ff606d796
syxxzzr <syxxzzr@163.com>
2025-03-22 00:51:47 +0800

    Update

--- functions/api/user/_middleware.js
+++ functions/api/user/_middleware.js
@@ -2,8 +2,12 @@ import {auth} from "../../utils.js";
 
 export function onRequest(context) {
     const assessKey = context.request.headers.get('Access-Key');
-    let { ok, errResponse } = auth(assessKey, context.env.accessKey);
-    if (!ok)
-        return errResponse;
+    let { ok } = auth(assessKey, context.env.accessKey);
+    if (!ok) {
+        const adminKey = context.request.headers.get('Admin-Key');
+        let { ok, errResponse } = auth(adminKey, context.env.adminKey);
+        if (!ok)
+            return errResponse;
+    }
     return context.next();
 }
