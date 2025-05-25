8368d56ae87fa1afc596f439f52adf06003f575f
syxxzzr <syxxzzr@163.com>
2025-05-26 00:09:17 +0800

    Update page title

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@ export async function onRequest(context) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js"></script>
-    <title>六班记忆</title>
+    <title>${env.projectName}</title>
     <style>
         * {
             margin: 0;
