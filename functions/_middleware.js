5bdfebb850ed2536738ae33a208523c901b002be
syxxzzr <syxxzzr@163.com>
2025-05-25 23:45:10 +0800

    Fix BUG

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,9 +1,9 @@
 export function onRequest(context) {
     if (!['accessKey', 'adminKey', 'tgBotToken', 'tgChannelID', 'KVSpace', 'projectName', 'projectDescription'].every(key => context.env.hasOwnProperty(key)))
-        return new Response(Response(JSON.stringify({
+        return new Response(JSON.stringify({
             ok: false,
             error_code: 400,
             description: 'Bad Request: server running without necessary configs'
-        }), { status: 400 }));
+        }), { status: 400 });
     return context.next();
 }
